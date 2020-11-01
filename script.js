7a7ab1e4499c0a4ebbe7ebe414b8477a0bf4ec85
Benjamin Smith <bbsmith1107@email.campbell.edu>
2020-10-31 21:32:47 -0400

    UV changes and buttons enabled

--- script.js
+++ script.js
@@ -129,6 +129,63 @@ $("#search-btn").on("click", function (){
                             <br />`
                             ;
                $("main").prepend(newDiv);
+
+               $("#new-btn").on("click", function (){
+                event.preventDefault();
+
+                var mynewDiv = $("newDiv") ;
+                let newDiv = `
+                            <h3>5-Day 3-Hour Forecast:</h3>
+                            <div class=card>
+                            <b> City: ${response.city.name}</b>
+                            <p>Date: ${response.list[6].dt_txt} GMT-400 (Eastern Daylight Time)</p>
+                            <h6>Tempurature: ${convertTemp1}*F (degrees Fahrenheit) </h6>
+                            <h6>             ${convertTemp001}*C (degrees Celsius) </h6>
+                            <h6>             ${response.list[6].main.temp} Kelvin </h6>
+                            <p>Humidity: ${response.list[6].main.humidity}%</p>
+                            </div>
+                            <br />
+                            <div class=card>
+                            <b> City: ${response.city.name}</b>
+                            <p>Date: ${response.list[14].dt_txt} GMT-400 (Eastern Daylight Time)</p>
+                            <h6>Tempurature: ${convertTemp2}*F (degrees Fahrenheit) </h6>
+                            <h6>             ${convertTemp002}*C (degrees Celsius) </h6>
+                            <h6>             ${response.list[14].main.temp} Kelvin </h6>
+                            <p>Humidity: ${response.list[14].main.humidity}%</p>
+                            </div>
+                            <br />
+                            <div class=card>
+                            <b> City: ${response.city.name}</b>
+                            <p>Date: ${response.list[22].dt_txt} GMT-400 (Eastern Daylight Time)</p>
+                            <h6>Tempurature: ${convertTemp3}*F (degrees Fahrenheit) </h6>
+                            <h6>             ${convertTemp003}*C (degrees Celsius) </h6>
+                            <h6>             ${response.list[22].main.temp} Kelvin </h6>
+                            <p>Humidity: ${response.list[22].main.humidity}%</p>
+                            </div>
+                            <br />
+                            <div class=card>
+                            <b> City: ${response.city.name}</b>
+                            <p>Date: ${response.list[30].dt_txt} GMT-400 (Eastern Daylight Time)</p>
+                            <h6>Tempurature: ${convertTemp4}*F (degrees Fahrenheit) </h6>
+                            <h6>             ${convertTemp004}*C (degrees Celsius) </h6>
+                            <h6>             ${response.list[30].main.temp} Kelvin </h6>
+                            <p>Humidity: ${response.list[30].main.humidity}%</p>
+                            </div>
+                            <br />
+                            <div class=card>
+                            <b> City: ${response.city.name}</b>
+                            <p>Date: ${response.list[38].dt_txt} GMT-400 (Eastern Daylight Time)</p>
+                            <h6>Tempurature: ${convertTemp5}*F (degrees Fahrenheit) </h6>
+                            <h6>             ${convertTemp005}*C (degrees Celsius) </h6>
+                            <h6>             ${response.list[38].main.temp} Kelvin </h6>
+                            <p>Humidity: ${response.list[38].main.humidity}%</p>
+                            </div>
+                            <br />`
+                            ;
+               $("main").prepend(newDiv);
+
+
+               })
     })
 
     //ajax call for current weather in given city
@@ -162,17 +219,21 @@ $("#search-btn").on("click", function (){
                 console.log(response.value);
                 if (response.value <= 5.00) {
                     console.log ("temperate")
-                    $("#card001").css("background-color", "goldenrod")
+                    $(".card001").css("background-color", "goldenrod")
                 }
                 if (response.value >= 5.01) {
                     console.log ("higher risk")
-                    $("#card001").css("background-color", "pink")
+                    $(".card001").css("background-color", "pink")
                 }
 
             //adding to mynewp
                 var mynewP = $("newP") ;
-                let newP = `<p> UV index number: ${response.value} </p>`;
-                $("#card001").prepend(newP);
+                let newP = 
+                `<div id = card><p>
+                UV Index Number: ${response.value}
+                </p></div>
+                <br />`;
+                $(".card001").prepend(newP);
         
             })
             
@@ -205,13 +266,69 @@ $("#search-btn").on("click", function (){
                              <h6>             ${response.main.temp} Kelvin </h6>
                              <p>Wind Speed: ${response.wind.speed} mph</p>
                              <p>Humidity: ${response.main.humidity}% </p>
-                             <div id= card001> </div>
                              </div>
                              <br/>`;
                  $("section").prepend(newDiv001);
+
+                 var mynewButton = $("newButton") ;
+                 let newButton = `<button id="new-btn" class="btn btn-success" type="submit">
+                                ${response.name}
+                                </button>
+                                <br />`
+                 $("#new-buttons").prepend(newButton);
+
+                 $("#new-btn").on("click", function (){
+                    event.preventDefault();
+
+                    console.log(response.value);
+                    
+                    var mynewDiv001 = $("newDiv001") ;
+                    let newDiv001= `<h3>Today's Weather:</h3>
+                                 <div class=card>
+                                 <b> City: ${response.name}</b> 
+                                 <p>Date: ${date}</p>                    
+                                 <h6>Tempurature: ${convertTemp}*F (degrees Fahrenheit) </h6>
+                                 <h6>             ${convertTemp000}*C (degrees Celsius) </h6>
+                                 <h6>             ${response.main.temp} Kelvin </h6>
+                                 <p>Wind Speed: ${response.wind.speed} mph</p>
+                                 <p>Humidity: ${response.main.humidity}% </p>
+                                 </div>
+                                 <br/>`;
+                     $("section").prepend(newDiv001);
+
+                     $.ajax({
+                        url: queryUV,
+                        method: "GET"
+                    }).then(function(response){
+                        console.log(response, "response")
+                        
+                    //binary conversion to advise viewer on UV protection 
+                        console.log(response.value);
+                        if (response.value <= 5.00) {
+                            console.log ("temperate")
+                            $(".card001").css("background-color", "goldenrod")
+                        }
+                        if (response.value >= 5.01) {
+                            console.log ("higher risk")
+                            $(".card001").css("background-color", "pink")
+                        }
+        
+                    //adding to mynewp
+                        var mynewP = $("newP") ;
+                        let newP = 
+                        `<div id = card><p>
+                        UV Index Number: ${response.value}
+                        </p></div>
+                        <br />`;
+                        $(".card001").prepend(newP);
+
+                
+                    })
+
+
+                 })
+
          })   
 
-    
 
-})
-
+        })
